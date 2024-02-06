0242534cff4007f5fa1130c204cfd8354a0f8da6
Francisco Diaz Herrero <95752970+franciscomdh@users.noreply.github.com>
2024-02-06 16:18:57 -0300

    a

--- js/others.js
+++
@@ -1,6 +0,0 @@
-
-
-document.getElementById('loadButton').addEventListener('click', function() {
-    var url =document.getElementById('urlInput').value;
-    document.getElementById('lastHit').src ="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/"+ url+"&amp;{ ADD YOUR PARAMETERS HERE }";
-});
